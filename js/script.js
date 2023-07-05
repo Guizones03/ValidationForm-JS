269c3c8a34f829f297072652d4a20970aef18694
Guilherme <souzaguitc@gmail.com>
2023-07-05 14:04:28 -0300

    Primeiro comite de projeto

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const form = document.querySelector('form')
+const name = document.getElementById('name')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const message = document.getElementById('message')
+
+form.addEventListener("submit", (event) =>{
+    event.preventDefault()
+});
