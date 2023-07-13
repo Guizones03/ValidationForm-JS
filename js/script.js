08e30d4ac205909f53b510d3362d6be9093904cb
Guilherme <souzaguitc@gmail.com>
2023-07-13 14:56:16 -0300

    Adição da funcionalidade de poupup para confirmação do cadastro pelo usuario

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ const vehicle = document.getElementById('vehicle')
 const inputs = document.querySelectorAll('input')
 const poupup = document.getElementById("confirm-poupup")
 const content = document.getElementById('contentPoupup')
+const main = document.getElementById('mainContent')
 
 nome.addEventListener('focus', function(){
     nome.placeholder = "Ex: João da Silva"
@@ -26,17 +27,37 @@ inputs.forEach(function(input){
         input.placeholder = ""
     })
 })
+const cancelbtn = document.getElementById('cancelBtn')
+cancelbtn.addEventListener('click' , a =>{
+    poupup.style.visibility = 'hidden'
+    main.classList.remove('poupup')
+})
+
+const confirmBtn = document.getElementById('confirmBtn')
+confirmBtn.addEventListener('click', b =>{
+    poupup.style.visibility = 'hidden'
+    
+})
 
 form.addEventListener('submit', function(ev){
     ev.preventDefault()
-
+    main.classList.add('poupup')
+    poupup.style.visibility = 'visible'
     content.innerText = `Nome: ${nome.value}
                         Telefone: ${tel.value}
                         Email: ${email.value}
                         Endereço: ${adress.value}
                         Veículo: ${vehicle.value}`
+    const client = {
+        name: nome.value,
+        phone:tel.value,
+        email:email.value,
+        adress: adress.value,
+        vehicle: vehicle.value
+    }
     inputs.forEach(function(input){
     input.value = ""
    })
 
 })
+
